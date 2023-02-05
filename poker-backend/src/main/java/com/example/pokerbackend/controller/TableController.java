4b899e00f9c8c0067ce7e29638c9da2f18e84b5e
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2023-02-05 20:06:53 +0100

    configure api controller interface class

--- poker-backend/src/main/java/com/example/pokerbackend/controller/TableController.java
+++ poker-backend/src/main/java/com/example/pokerbackend/controller/TableController.java
@@ -1,6 +1,6 @@
 package com.example.pokerbackend.controller;
 
-import com.example.pokerapi.openapi.api.TablesApi;
+import com.example.pokerapi.openapi.api.TableApi;
 import com.example.pokerapi.openapi.model.TableDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.List;
 
 @RestController
-public class TableController implements TablesApi {
+public class TableController implements TableApi {
 
     @Override
     public ResponseEntity<List<TableDto>> allTables() {
