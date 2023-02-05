3c6a8ebfdb109df4ae8daaa34a4178d56a4c1080
Mariusz Kleczar <kleczar.mariusz@gmail.com>
2023-02-05 17:01:46 +0100

    initial revision

---
+++ poker-backend/src/main/java/com/example/pokerbackend/PokerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerBackendApplication.class, args);
+    }
+
+}
